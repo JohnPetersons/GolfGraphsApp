aedd72f675f1f63a5f169cce741fe2f3b608d8c0
JohnPetersons <john.w.petersons@gmail.com>
2024-11-26 11:14:33 -0500

    missed a file last commit

---
+++ src/views/ItemInlineInput.tsx
@@ -0,0 +1,36 @@
+import {
+    useState,
+    useContext
+    } from 'react';
+import {
+    Text,
+    View,
+    TextInput,
+    Button,
+    } from 'react-native';
+import type {PropsWithChildren} from 'react';
+import styles from "../styles/itemStyles"; 
+import { DataContext } from '../../App';
+
+export type ItemInlineInputProps = PropsWithChildren<{
+    label: string,
+    fncs: {
+        // getDataFnc: () => string,
+        // setDataFnc: (data: any) => string
+    }
+}>;
+
+export function ItemInlineInput({label, fncs}: ItemInlineInputProps): React.JSX.Element {
+
+    const {data, setData} = useContext(DataContext);
+
+    return (
+        <View style={styles.itemContainer}>
+            <View style={styles.itemBuffer}></View>
+            <Text style={styles.itemLabel}>
+            {label}
+            </Text>
+            <TextInput style={styles.itemInput} onChangeText={setData}>{data()}</TextInput>
+        </View>
+    );
+}
